e4d12ba41098cf51404fae051fe863a0702a498a
Олег <OllegOzz@yandex.ru>
2023-12-09 11:22:13 +0300

    1

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@ import React from "react";
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './components/App/App'
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(<App/>)
+createRoot(document.getElementById('root')).render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+
+
+)
