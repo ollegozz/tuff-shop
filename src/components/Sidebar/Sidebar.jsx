615e4232ba63fb298cf3f603ec52b2c44898a679
Олег <OllegOzz@yandex.ru>
2023-12-10 10:44:04 +0300

    Sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -3,8 +3,40 @@ import styles from '../../styles/Sidebar.module.css'
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
-  )
+    <section className={styles.sidebar}>
+      <div className={styles.title}>CATEGORIES</div>
+      <nav>
+        <ul className={styles.menu}>
+          {list.map(({ id, name }) => (
+            <li key={id}>
+              <NavLink
+                className={({ isActive }) =>
+                  `${styles.link} ${isActive ? styles.active : ""}`
+                }
+                to={`/categories/${id}`}
+              >
+                {name}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+
+      <div className={styles.footer}>
+        <a href="/help" target="_blank" className={styles.link}>
+          Help
+        </a>
+        <a
+          href="/terms"
+          target="_blank"
+          className={styles.link}
+          style={{ textDecoration: "underline" }}
+        >
+          Terms  Conditions
+        </a>
+      </div>
+    </section>
+  );  
 }
 
 export default Sidebar
