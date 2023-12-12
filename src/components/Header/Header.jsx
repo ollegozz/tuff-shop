e0a45e8f06d3da7c4b8a5afd842efdda6c490787
Олег <OllegOzz@yandex.ru>
2023-12-12 17:39:33 +0300

    img

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { ROUTES } from "../../utils/routes";
 import styles from '../../styles/Header.module.css'
+import logo from '../../images/logo.svg'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className={styles.header}>
+      <div className={styles.logo}>
+        <Link to={ROUTES.HOME}>
+          <img src={logo} alt="Stuff" />
+        </Link>
+      </div>
+      <div className={styles.info}>
+        
+      </div>
+    </div>
   )
 }
 
