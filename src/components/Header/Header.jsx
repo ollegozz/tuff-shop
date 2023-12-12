3725bfb384a600ed4dae448bbd4467f15da3e22c
Олег <OllegOzz@yandex.ru>
2023-12-12 17:47:06 +0300

    Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import { ROUTES } from "../../utils/routes";
 import styles from '../../styles/Header.module.css'
 import logo from '../../images/logo.svg'
+import avatar from '../../images/avatar.jpg'
 
 const Header = () => {
   return (
@@ -13,7 +14,14 @@ const Header = () => {
         </Link>
       </div>
       <div className={styles.info}>
-        
+        <div className={styles.user}></div>
+        <div className={styles.user}>
+          <div className={styles.avater} style={{ backgroundImage: `url(${avatar})`}}/>
+          <div className={styles.username}>Guest</div>
+        </div>
+        <form className={styles.form}>
+          <div className={styles.icon}></div>
+        </form>
       </div>
     </div>
   )
